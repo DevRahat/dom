6590b2344c239fae4bfb481e9bce2ecc77f31186
DevRahat <dev.rahatislam@gmail.com>
2024-02-11 01:00:59 +0600

    dom02

--- js/dom.js
+++ js/dom.js
@@ -4,4 +4,13 @@ for(const section of sections){
     section.style.marginBottom= '5px';
     section.style.borderRadius= '5px';
     section.style.padding= '10px';
+    section.style.backgroundColor= 'lightgray';
 }
+// const placesContainer=document.getElementById('places-container');
+//     placesContainer.style.backgroundColor='yellow';
+
+
+//add class name using dom
+const placesContainer=document.getElementById('places-container');
+placesContainer.classList.add('yellow-bg');
+placesContainer.classList.remove('yellow-bg');
