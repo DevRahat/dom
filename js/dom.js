f6a8dbeeb59d0f09ca0a67153d5a18be251e7d1c
DevRahat <dev.rahatislam@gmail.com>
2024-02-11 00:36:30 +0600

    dom

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,7 @@
-console.log('Hello from Dom');
+const sections=document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border='2px solid blue';
+    section.style.marginBottom= '5px';
+    section.style.borderRadius= '5px';
+    section.style.padding= '10px';
+}
