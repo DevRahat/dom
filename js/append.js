f7f81423d2de10a8a77391c29c474eca47d5d4d0
DevRahat <dev.rahatislam@gmail.com>
2024-02-11 20:12:34 +0600

    last

---
+++ js/append.js
@@ -0,0 +1,49 @@
+//1.Where to add
+const placesList=document.getElementById('places-list')
+//2.what to be added
+const li=document.createElement('li');
+li.innerText='Coxbazar';
+
+//3. add the child 
+placesList.appendChild(li);
+
+//1)Where to add
+const mainContainer=document.getElementById('main-container');
+
+
+const section=document.createElement('section');
+const h1=document.createElement('h1');
+h1.innerText=("My Food List");
+section.appendChild(h1);
+const ul=document.createElement('ul');
+
+const li1=document.createElement('li');
+li1.innerText='Briyani'
+ul.appendChild(li1);
+
+const li2=document.createElement('li');
+li2.innerText='Borhani'
+ul.appendChild(li2);
+
+
+
+const li3=document.createElement('li');
+li3.innerText='Salad';
+ul.appendChild(li3);
+
+section.appendChild(ul);
+mainContainer.appendChild(section);
+
+
+const sectionDress=document.createElement('section');
+sectionDress.innerHTML=`
+<h1>
+My Dress section
+</h1>
+<ul>
+    <li>T-Shirt</li>
+    <li>pant</li>
+    <li>Sando genji</li>
+</ul>
+`
+mainContainer.appendChild(sectionDress);
